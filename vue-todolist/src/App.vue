427a0d10084eef56f943bfe47bf9b247f6263fa6
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-07-10 14:32:02 -0300

    Adicionado o app todolist

---
+++ vue-todolist/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <div class="ui inverted vertical masthead center aligned segment">
+      <div class="ui text container">
+        <h1 class="ui inverted header"> Aplicação Todo List com Vue.js 3</h1>
+        <h2 class="ui inverted header">Lucas de Oliveira Macedo
+          <div class="sub header">Bacharel em Ciência da Computação</div>
+        </h2>
+        <h3 class="ui inverted header">Aracaju, Brasil - 2020</h3>
+        <a class="tiny ui youtube button" type="button" href="https://www.youtube.com/c/LucasdeOliveiraMacedo">
+          <i class="youtube icon"></i>
+          YouTube
+        </a>
+        <a class="tiny ui linkedin button" type="button" href="https://www.linkedin.com/in/lucasomac/">
+          <i class="linkedin icon"></i>
+          LinkedIn
+        </a>
+        <a class="tiny ui twitter button" type="button" href="https://twitter.com/lucasomac">
+          <i class="twitter icon"></i>
+          Twitter
+        </a>
+      </div>
+    </div>
+    <br />
+    <div class="ui three column centered grid">
+      <div class="column">
+        <todo-list v-bind:todos="todos"></todo-list>
+        <create-todo v-on:create-todo="createTodo"></create-todo>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import sweetAlert from 'sweetalert';
+import TodoList from './components/pages/TodoList/index';
+import CreateTodo from './components/pages/CreateTodo/index';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          titulo: 'Todo A',
+          projeto: 'Projeto A',
+          concluido: false,
+        },
+        {
+          titulo: 'Todo B',
+          projeto: 'Projeto B',
+          concluido: true,
+        },
+        {
+          titulo: 'Todo C',
+          projeto: 'Projeto C',
+          concluido: false,
+        },
+        {
+          titulo: 'Todo D',
+          projeto: 'Projeto D',
+          concluido: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    createTodo(novoTitulo) {
+      this.todos.push(novoTitulo);
+      sweetAlert('Sucesso!', 'Nova Tarefa Adicionada.', 'success');
+    },
+  },
+};
+</script>
